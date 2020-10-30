a48eff4aa624921229b3dc8b4785935a83724737
SHALINI J <shalini1307@outlook.com>
2020-10-30 13:17:40 +0530

    new line

--- Hello world/Program.cs
+++ Hello world/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Hello_world
@@ -10,7 +11,8 @@ namespace Hello_world
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello world");     
+            System.Console.WriteLine("Hello world");
+            System.Console.WriteLine("hi");
         }
     }
 }
